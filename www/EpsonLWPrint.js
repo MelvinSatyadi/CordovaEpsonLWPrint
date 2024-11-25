664e7057a2cbc4bd9a317a2d63d39270e7eb3e9c
Melvin Satyadi <melvin.satyadi+pub@gmail.com>
2024-11-25 14:46:40 +0700

    fixed case

--- www/EpsonLWPrint.js
+++ www/EpsonLWPrint.js
@@ -14,6 +14,6 @@ EpsonLWPrint.install = function () {
   if (!window.plugins) {
     window.plugins = {};
   }
-  window.plugins.EpsonLWPrint = new EpsonLWPrint();
+  window.plugins.epsonLWPrint = new EpsonLWPrint();
   return window.plugins.EpsonLWPrint;
 };
